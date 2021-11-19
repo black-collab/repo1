a59c7013b203ff6aa415e8f127ab0342a01038e1
lk <123456@qq.com>
2021-11-19 18:18:21 +0800

    m1测试

--- src/test/java/com/itheima/test/AccountServiceTest.java
+++ src/test/java/com/itheima/test/AccountServiceTest.java
@@ -16,6 +16,7 @@ public class AccountServiceTest {
 
     @Test
     public void testF() {
+        //m1
         HashMap<String, String> map = new HashMap<String, String>(2);
         map.put("u","你好");
         map.put("v","你好");
